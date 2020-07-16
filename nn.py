821e8460e9633d066bc66b104012af3a50c48626
Caio Andrade <caiotdandrade@gmail.com>
2020-07-16 09:58:33 -0300

    Drafts neural network and training implementations

---
+++ nn.py
@@ -0,0 +1,16 @@
+from torch import nn, optim
+import torch.nn.functional as F
+
+class Net_2hl(nn.Module):
+    def __init__(self, n_features, n_hl1, n_hl2):
+        super(Net_2hl, n_features, n_hl1, n_hl2)
+        self.hl1 = nn.Linear(n_features, n_hl1)
+        self.hl2 = nn.Linear(n_hl1, n_hl2)
+        self.output_layer = nn.Linear(n_hl2, 1)
+
+    def forward(self, x):
+        h1 = F.relu(self.hl1(x))
+        h2 = F.relu(self.hl2(h1))
+        y = F.sigmoid(self.output_layer(h2))
+
+        return y
---
+++ train.py
@@ -0,0 +1,42 @@
+import numpy as np
+from sklearn.metrics import confusion_matrix, classification_report
+import torch
+
+class Train():
+    def __init__(self, net, lr, x_train, x_test, y_test, epochs):
+
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+        self.net = net.to(device)
+        self.x_train = x_train.to(device)
+        self.x_test = x_test.to(device)
+        self.y_test = y_test.to(device)
+        self.epochs = epochs
+        self.criterion = torch.nn.BCELoss()
+        self.optimizer = torch.optim.Adam(net.parameters(), lr=lr)
+
+
+
+    def calculate_accuracy(self, y_true, y_pred):
+        predicted = y_pred.ge(.5).view(-1)
+        return (y_true == predicted).sum().float() / len(y_true)
+
+    def train(self):
+        for epoch in range(self.epochs):
+            y_pred = self.net(self.x_train)
+            y_pred = torch.squeeze(y_pred)
+            # Precisa disso?
+            train_loss = self.criterion(y_pred, self.y_test)
+            if epoch % 100 == 0:
+                train_acc = self.calculate_accuracy(self.y_test, y_pred)
+                y_test_pred = self.net(self.x_test)
+                y_test_pred = torch.squeeze(y_test_pred)
+                test_loss = self.criterion(y_test_pred, self.y_test)
+                test_acc = self.calculate_accuracy(self.y_test, y_test_pred)
+                print('EPOCH {0}:'.format(epoch))
+                print('Train Set --- loss:{0}; acc:{1}'.format(train_loss, train_acc))
+                print('Validation Set  --- loss:{0}; acc:{1}'.format(test_loss, test_acc))
+            
+            self.optimizer.zero_grad()
+            train_loss.backward()
+            self.optimizer.step()
