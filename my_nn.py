69bb1463e3f96a448cd6ed999de25f5e2418a74d
Caio Andrade <caiotdandrade@gmail.com>
2020-07-19 11:46:06 -0300

    renames nn.py to my_nn.py and correctly feeds neural network with data

--- my_nn.py
+++ my_nn.py
@@ -3,10 +3,11 @@ import torch.nn.functional as F
 
 class Net_2hl(nn.Module):
     def __init__(self, n_features, n_hl1, n_hl2):
-        super(Net_2hl, n_features, n_hl1, n_hl2)
+        super(Net_2hl, self).__init__()
         self.hl1 = nn.Linear(n_features, n_hl1)
         self.hl2 = nn.Linear(n_hl1, n_hl2)
-        self.output_layer = nn.Linear(n_hl2, 1)
+        self.output_layer = nn.Linear(n_hl2, 3) #Tres possiveis saidas: classificação
+        # em positivo, negativo ou indeterminado
 
     def forward(self, x):
         h1 = F.relu(self.hl1(x))
